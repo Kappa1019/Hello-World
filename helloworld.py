61c61a40670928f180575d49fba327c5630b5aa0
Kappa1019 <mishradevansh2005@gmail.com>
2025-03-18 20:26:24 +0530

    updated the code

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,1 @@
-'''
-
-TO UPLOAD FILE FROM LOCAL SYSTEM TO GITHUB
-git init
-git remote add origin https://github.com/Kappa1019/Hello-World.git
-git add filename.ext
-git commit -m "Added helloworld.py"
-git branch
-git push origin main
-
-'''
+print("Hello World!")
