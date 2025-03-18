515efabeb37e9951b84e3b2c488d376bce39bf7c
Kappa1019 <mishradevansh2005@gmail.com>
2025-03-18 20:20:21 +0530

    updated change

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+'''
+
+to upload file to github
+git init
+git remote add origin https://github.com/Kappa1019/Hello-World.git
+git add filename.ext
+git commit -m "Added helloworld.py"
+git branch
+git push origin main
+
+'''
